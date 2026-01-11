4c88266b319e7e7e2305301b40b785d3c5e05beb
SgnCycles <signe.bekere@gmail.com>
2026-01-11 14:41:46 +0100

    add skills section

---
+++ scripts/skills.js
@@ -0,0 +1,12 @@
+const skillsSection = document.querySelector('.skills-card-container');
+
+const listOfSkills = ['HTML', 'CSS', 'Sass', 'Javascript', 'Version Control', 'Git', 'GitHub', 'Responsive Design', 'jQuery', 'Bootstrap', 'Testing', 'Debugging', 'Lighthouse', 'APIs'];
+
+
+listOfSkills.forEach(skill => {
+  const card = document.createElement('div');
+  card.classList.add('skill-card');
+  card.textContent = skill;
+
+  skillsSection.appendChild(card);
+})
